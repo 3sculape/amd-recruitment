982414298c9cc2170f0a30ad71fa4002b06345c4
Samuel Quesne <samu3lquesne@gmail.com>
2023-11-24 15:36:23 +0100

    feat(action): change to install script

---
+++ .github/actions/run-python-venv/install-script.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+touch /tmp/constraints.txt
+
+if [ -n "$3" ]
+then
+  cp "$3" /tmp/constraints.txt
+fi
+
+if [ -n "$4" ]
+then
+  echo "$4" > /tmp/constraints.txt
+fi
+
+if [ -n "$1" ]
+then
+  pip install -c /tmp/constraints.txt -r "$1" "$2"
+else
+  pip install -c /tmp/constraints.txt "$2"
+fi
