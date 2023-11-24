68f0627005a319fd631a7d1347e5bb0026fbef8f
Samuel Quesne <samu3lquesne@gmail.com>
2023-11-24 17:55:03 +0100

    fix: base behaviour

--- .github/scripts/update-checkbox.py
+++ .github/scripts/update-checkbox.py
@@ -12,26 +12,19 @@ def update_checkbox(token, repo_name, pr_number):
     # Get the pull request
     pr = repo.get_pull(int(pr_number))
 
-    # Get the comments on the pull request
-    comments = pr.get_issue_comments()
-
-    # Regular expression to match the mentioned pull request format
-    pr_mention_pattern = re.compile(r'#(\d+)')
+    # Get the events on the pull request
+    events = pr.as_issue().get_timeline()
 
     # Loop through the comments
-    for comment in comments:
-        # Check if the comment contains a mentioned pull request
-        matches = pr_mention_pattern.findall(comment.body)
-        for match in matches:
-            mentioned_pr_number = int(match)
+    for event in events:
+        if event.event == "cross-referenced":
+            mentioned_pr_number = event.source.number
             print(mentioned_pr_number)
-
-            # Check if the mentioned pull request is closed
-            mentioned_pr = repo.get_pull(mentioned_pr_number)
-            if mentioned_pr.state == 'closed':
-                # Update the original comment with a checked checkbox
-                updated_body = comment.body.replace(f"[ ] PR #{pr_number}", f"[x] PR #{pr_number}")
-                comment.edit(body=updated_body)
+            comments = event.source.get_issue_comments()
+            for comment in comments:
+                if f"[ ] PR #{pr_number}" in comment.body
+                    updated_body = comment.body.replace(f"[ ] PR #{pr_number}", f"[x] PR #{pr_number}")
+                    comment.edit(body=updated_body)
 
 if __name__ == "__main__":
     repo_name = os.getenv('REPO_NAME')
