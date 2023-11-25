22d86794aeb1089b686288951f835cf9e5d3f6ef
Samuel Quesne <samu3lquesne@gmail.com>
2023-11-25 16:03:40 +0100

    chore: add comments

--- .github/scripts/run-python-venv/install-script.sh
+++ .github/scripts/run-python-venv/install-script.sh
@@ -1,17 +1,26 @@
-#/bin/bash
+#!/bin/bash
+
+## Inputs
+# 1 Requirements file path
+# 2 Requirements line
+# 3 Constraints file path
+# 4 Constraints line
 
 touch /tmp/constraints.txt
 
+# Add constraints file content to the tmp file
 if [ -n "$3" ]
 then
   cp "$3" /tmp/constraints.txt
 fi
 
+# Add constraints content to the tmp file
 if [ -n "$4" ]
 then
-  echo "$4" > /tmp/constraints.txt
+  echo "$4" >> /tmp/constraints.txt
 fi
 
+# Run the pip install command with the constraints
 if [ -n "$1" ]
 then
   pip install -c /tmp/constraints.txt -r "$1" $2
--- .github/scripts/update-checkbox.py
+++ .github/scripts/update-checkbox.py
@@ -12,14 +12,16 @@ def update_checkbox(token, repo_name, pr_number):
     # Get the pull request
     pr = repo.get_pull(int(pr_number))
 
-    # Get the events on the pull request
+    # Get the timeline events on the pull request
     events = pr.as_issue().get_timeline()
 
-    # Loop through the comments
+    # Loop through the events
     for event in events:
         if event.event == "cross-referenced":
+            # Found a cross-reference
             mentioned_pr = event.source.issue
             comments = mentioned_pr.get_comments()
+            # Search for checkbox and ticks it
             for comment in comments:
                 if f"[ ] PR #{pr_number}" in comment.body:
                     updated_body = comment.body.replace(f"[ ] PR #{pr_number}", f"[x] PR #{pr_number}")
